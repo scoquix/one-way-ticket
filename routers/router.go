dd8182cea0c9afaaf23ebb7dd7f4407c3b83f9ea
scoquix <sebaszafranski@gmail.com>
2024-05-22 01:26:50 +0200

    Removed unnecessary health endpoint

--- routers/router.go
+++ routers/router.go
@@ -1,24 +1,14 @@
 package routers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"one-way-ticket/auth"
 	"one-way-ticket/users"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	r.POST("/health", func(c *gin.Context) {
-		// Get name from query parameter (optional)
-		name := c.Query("name")
-		if name != "" {
-			c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Hello, %s!", name)})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
-		}
-	})
+
 	r.POST("/login", auth.Login)
 
 	userRoutes := r.Group("/users")
