d7f09c0313476fa9f6c6cc3c1b2894aa09edcf80
scoquix <sebaszafranski@gmail.com>
2024-05-22 01:14:28 +0200

    Added integration test for Dynamo and Docker compose

---
+++ dynamo/dynamo_test.go
@@ -0,0 +1,82 @@
+//go:build integration_test
+// +build integration_test
+
+package dynamo
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTable(t *testing.T) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-east-1"),
+		config.WithEndpointResolver(
+			aws.EndpointResolverFunc(
+				func(service, region string) (aws.Endpoint, error) {
+					if service == dynamodb.ServiceID && region == "us-east-1" {
+						return aws.Endpoint{
+							URL: "http://host.docker.internal:4566",
+							//URL: "http://localhost:4566",
+						}, nil
+					}
+					return aws.Endpoint{}, fmt.Errorf("unknown endpoint requested")
+				},
+			),
+		),
+	)
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	svc := dynamodb.NewFromConfig(cfg)
+
+	tableName := "ExampleTable"
+	input := &dynamodb.CreateTableInput{
+		TableName: aws.String(tableName),
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String("ID"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String("ID"),
+				KeyType:       types.KeyTypeHash, // Partition key
+			},
+		},
+		ProvisionedThroughput: &types.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(5),
+			WriteCapacityUnits: aws.Int64(5),
+		},
+	}
+
+	_, err = svc.CreateTable(context.TODO(), input)
+	assert.NoError(t, err, "failed to create table")
+
+	fmt.Println("Waiting for table to be created...")
+
+	waiter := dynamodb.NewTableExistsWaiter(svc)
+	err = waiter.Wait(context.TODO(), &dynamodb.DescribeTableInput{
+		TableName: aws.String(tableName),
+	}, 5*time.Minute)
+	assert.NoError(t, err, "failed to wait for table creation")
+
+	fmt.Println("Table created successfully")
+
+	// Clean up
+	_, err = svc.DeleteTable(context.TODO(), &dynamodb.DeleteTableInput{
+		TableName: aws.String(tableName),
+	})
+	assert.NoError(t, err, "failed to delete table")
+}
